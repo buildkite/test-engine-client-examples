5786a756364c80687a90193ae78b155ae67f288c
Naufan Rizal <naufan@buildkite.com>
2024-10-29 14:33:28 +1300

    Create Playwright sample project

---
+++ playwright/playwright.config.js
@@ -0,0 +1,23 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ */
+module.exports = defineConfig({
+  testDir: './tests',
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ],
+});
+
