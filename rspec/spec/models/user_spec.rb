72ebf6aa904b93fdf897ff1dba79d3ab9fc55b1a
Paul Annesley <paul@annesley.cc>
2025-02-21 14:26:37 +1030

    Testing per-execution tags via Ruby collector

--- rspec/spec/models/user_spec.rb
+++ rspec/spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require "spec_helper"
 
 RSpec.describe "User" do
   it "has a name" do
+    Buildkite::TestCollector.tag_execution("custom.tag.from", "execution")
     expect(true).to be true
   end
 end
