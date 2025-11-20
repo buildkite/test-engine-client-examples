5f330638c743500aa764b719e2b8ae1ed4b9c9f7
Gordon Chan <developer.gordon+github@gmail.com>
2025-11-21 10:53:58 +1300

    [TE-4941] Attempt to completely replicate rippling's environment

--- pytest-xdist/setup.py
+++ pytest-xdist/setup.py
@@ -9,9 +9,14 @@ setup(
     "dev": [
       "pytest==8.4.2",
       "pytest-xdist==3.8.0",
-      "pytest-rerunfailures==12.0.0",
-      "pytest-repeat",
-      "buildkite-test-collector>=1.1.0",
+      "pytest-rerunfailures==12.0",
+      "pytest-repeat==0.9.4",
+      "buildkite-test-collector==1.1.1",
+      "pytest-asyncio==1.2.0",
+      "pytest-timeout==2.4.0",
+      "anyio==4.11.0",
+      "langsmith==0.4.39",
+      "ddtrace==3.17.2",
     ],
   },
 )
