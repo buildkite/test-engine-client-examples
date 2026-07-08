1cce93f6b1bd73aeefb7d226fa2904a2ec3117cb
Naufan Rizal <naufan@buildkite.com>
2026-07-09 10:30:57 +1200

    Consolidate pytest and pytest-xdist

--- pytest-xdist/tests/test_substract.py
+++
@@ -1,7 +0,0 @@
-class TestSubstract:
-  def test_positive(self):
-    assert 5 - 3 == 2
-
-  def test_negative(self):
-    assert 3 - 5 == -2
-
--- pytest/tests/conftest.py
+++ pytest/tests/conftest.py
@@ -1,10 +1,13 @@
+import os
+
 import pytest
 
 def pytest_itemcollected(item):
   # add execution tag to all tests
   item.add_marker(pytest.mark.execution_tag("test.framework.name", "pytest"))
   item.add_marker(pytest.mark.execution_tag("test.framework.version", pytest.__version__))
-  item.add_marker(pytest.mark.execution_tag("test.framework.xdist", "false"))
+  xdist_enabled = "true" if os.environ.get("PYTEST_XDIST_ENABLED") == "true" else "false"
+  item.add_marker(pytest.mark.execution_tag("test.framework.xdist", xdist_enabled))
 
   # prepend this marker so it doesn't override the mark set in test decorator
   item.add_marker(pytest.mark.execution_tag("custom.tag.from", "hook"), False)
